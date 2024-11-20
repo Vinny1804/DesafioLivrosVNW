c3cf8eab59a6a945b3f8d87cd5b63838e3682618
Vinicius <vinny_aguiar@hotmail.com>
2024-11-20 15:53:54 -0300

    :sparkles: feat: Página Livros Doados

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,5 +1,23 @@
+import s from './livrosDoados.module.scss'
+import livro from '../../assets/livroOProtagonista.png'
+
 export default function LivrosDoados() {
     return (
-        <h2>LIVROS DOADOS</h2>
+        <section className={s.livrosDoados}>
+            <h2>Livros Doados</h2>
+
+            {/*section'listaDeLivros' criada para adicionar futuros livros*/}
+            <section className={s.listaDeLivros}>
+
+                <section className={s.Livro1}>
+                    <img src={livro} alt="Imagem da capa do livro: O protagonista"/>
+                    <div className={s.infoLivro}>
+                        <p id='titulo'>O protagonista</p>
+                        <p id='autor(a)'>Susanne Andrade</p>
+                        <p id='genero'>Ficção</p>
+                    </div>
+                </section>
+            </section>
+        </section>
     )
 }
