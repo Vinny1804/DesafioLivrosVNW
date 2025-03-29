9d75450645948fef35b2f72db777571bc12165e6
Vinicius <vinny_aguiar@hotmail.com>
2025-03-29 19:45:31 -0300

    :sparkles: feat: funcoes para captura e envio de dados para API

--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,7 +1,44 @@
 import s from './queroDoar.module.scss'
 import logoFormulario from '../../assets/logoFormulario.png'
+import { useState } from 'react'
+import axios from 'axios'
 
 export default function QueroDoar() {
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [image_url, setImage_url] = useState("")
+
+    const capturaTitulo = (e) => {
+        setTitulo(e.target.value);
+    }
+
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) => {
+        setAutor(e.target.value)
+    }
+
+    const capturaImage_Url = (e) => {
+        setImage_url(e.target.value)
+    }
+
+    const enviarDados = async() => {
+
+        const endPointApi = "https://api-livros-lb7r.onrender.com/doar"
+
+        const dadosAEnviar = {
+            titulo,
+            categoria,
+            autor,
+            image_url
+        }
+
+        await axios.post(endPointApi, dadosAEnviar)
+    }
+
     return (
         <section className={s.queroDoar}>
             <h3>Por favor, preencha o formulário com suas informações e as informações do Livro</h3>
@@ -12,26 +49,25 @@ export default function QueroDoar() {
                     <img src={logoFormulario} alt="Logo azul em formato de um livro aberto " />
                     <h3>Informações do Livro</h3>
                 </div>
-                <form className={s.forms} action="">
 
+                <form className={s.forms} onSubmit={(e) => e.preventDefault()}>
                         <fieldset>
-                            <input className={s.inputsFormulario} type="text" placeholder='Titulo'/>
+                            <input className={s.inputsFormulario} type="text" placeholder='Titulo' onChange={capturaTitulo}/>
                         </fieldset>
 
                         <fieldset>
-                            <input className={s.inputsFormulario} type="text" placeholder='Categoria'/>
+                            <input className={s.inputsFormulario} type="text" placeholder='Categoria' onChange={capturaCategoria}/>
                         </fieldset>
 
                         <fieldset>
-                            <input className={s.inputsFormulario} type="text" placeholder='Autor'/>
+                            <input className={s.inputsFormulario} type="text" placeholder='Autor' onChange={capturaAutor}/>
                         </fieldset>
 
                         <fieldset>
-                            <input className={s.inputsFormulario} type="text" placeholder='Link da Imagem'/>
+                            <input className={s.inputsFormulario} type="text" placeholder='Link da Imagem' onChange={capturaImage_Url}/>
                         </fieldset>
 
-                        <input className={s.inputButton} type="submit" value='Doar'/>
-
+                        <input className={s.inputButton} type="submit" value='Doar' onClick={enviarDados}/>
                 </form>
             </section>
 
